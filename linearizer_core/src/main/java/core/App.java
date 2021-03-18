69db53f95b95ec10bd83d87b623b51f16842478a
nikita.makarevich@spbpu.com <nikita.makarevich@spbpu.com>
2021-03-18 15:31:35 +0300

    initial commit

---
+++ linearizer_core/src/main/java/core/App.java
@@ -0,0 +1,9 @@
+package core;
+
+public class App {
+    public static void main(String[] args) {
+        //System.out.println(new App().getGreeting());
+        Main main = new Main();
+        main.openrepo();
+    }
+}
