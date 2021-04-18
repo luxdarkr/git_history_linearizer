4eaf4449eccc1a454cb3bbb630adf301eb2ed9ab
Kirill Parshukov <kirill.parshukov@mytona.com>
2021-04-18 05:58:08 -0700

    copy project files to core

---
+++ linearizer_core/src/main/java/core/Linearizer.java
@@ -0,0 +1,84 @@
+package git_lin;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import java.util.Dictionary;
+import java.util.List;
+
+public class Linearizer {
+    Repository activeRepo = null;
+    Pair<RevCommit, RevCommit> lastResultCommits = null;
+
+    Linearizer(Repository repo) {
+        activeRepo = repo;
+    }
+
+    public Pair getLastResultCommits() throws NullPointerException {
+        return lastResultCommits;
+    }
+
+    public void changeRepo(Repository repo) throws NullPointerException {
+        if (repo == null) {
+            throw new NullPointerException();
+        }
+        activeRepo = repo;
+        lastResultCommits = null;
+    }
+
+    public Pair linearize(RevCommit start, RevCommit end) throws Exception {
+        String branchName = "";
+        // TODO: build branch based on repo head name
+        branchName = "result";
+        return linearize(start, end, branchName);
+    }
+
+    public Pair linearize(RevCommit start, RevCommit end, String newBranchName) throws Exception {
+        if (start == null || end == null || newBranchName == null) {
+            throw new NullPointerException();
+        }
+        // TODO check branch name
+        // TODO implement
+        return lastResultCommits;
+    }
+
+    public Pair squash(RevCommit start, RevCommit end) throws NullPointerException {
+        if (start == null || end == null) {
+            throw new NullPointerException();
+        }
+        // TODO implement
+        return lastResultCommits;
+    }
+
+    public Pair stripCommitMessages(RevCommit start, RevCommit end) throws NullPointerException {
+        if (start == null || end == null) {
+            throw new NullPointerException();
+        }
+        // TODO implement
+        return lastResultCommits;
+    }
+
+    public Pair fixCaseInCommitMessages(RevCommit start, RevCommit end) throws NullPointerException {
+        if (start == null || end == null) {
+            throw new NullPointerException();
+        }
+        // TODO implement
+        return lastResultCommits;
+    }
+
+    public Pair removeStarsAndPlusesInCommitMessages(RevCommit start, RevCommit end) throws NullPointerException {
+        if (start == null || end == null) {
+            throw new NullPointerException();
+        }
+        // TODO implement
+        return lastResultCommits;
+    }
+
+    public Pair fixBigCommitMessages(RevCommit start, RevCommit end) throws NullPointerException {
+        if (start == null || end == null) {
+            throw new NullPointerException();
+        }
+        // TODO implement
+        return lastResultCommits;
+    }
+}
--- linearizer_core/src/main/java/core/Main.java
+++ linearizer_core/src/main/java/core/Main.java
@@ -1,4 +1,4 @@
-package core;
+package git_lin;
 
 import java.io.IOException;
 import java.io.File;
@@ -12,44 +12,49 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 
-// Parts taken from this repo:
+// Code taken from here
 // https://github.com/centic9/jgit-cookbook/tree/master/src/main/java/org/dstadler/jgit
 
 public class Main {
-    public void openrepo() {
-        // Путь к репозиторию (пока хардкод)
-        File repoDir = new File("C:/rapidjson/.git");
-        // File repoDir = new File("C:/Users/DDRDmakar/Documents/EDU/ТРПО/Tamagotchi-Atmega8");
+	public void openrepo(String repoPath) {
+		File repoDir = new File(repoPath);
 
-        // Creating repo object
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        try (Repository repository = builder.setGitDir(repoDir)
-                .readEnvironment() // Scan environment GIT_* variables
-                .findGitDir()      // Scan up the file system tree
-                .build()) {
-            System.out.println("Having repository: " + repository.getDirectory());
+		// Creating repo object
+		FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		try (Repository repository = builder.setGitDir(repoDir)
+				.readEnvironment() // Scan environment GIT_* variables
+				.findGitDir()      // Scan up the file system tree
+				.build()) {
+			System.out.println("Having repository: " + repository.getDirectory());
 
-            // The Ref holds an ObjectId for any type of object (tree, commit, blob, tree)
-            Ref head = repository.findRef("refs/heads/master");
-            System.out.println("Found head: " + head);
+			// The Ref holds an ObjectId for any type of object (tree, commit, blob, tree)
+			final String refName = "refs/heads/master";
+			Ref head = repository.findRef(refName);
+			if (head != null) {
+				System.out.println("Found head: " + head);
+			} else {
+				System.out.println("Cannot find head " + refName);
+			}
 
-            // A RevWalk allows to walk over commits based on some filtering that is defined
-            try (RevWalk walk = new RevWalk(repository)) {
-                RevCommit commit = walk.parseCommit(head.getObjectId());
+			// A RevWalk allows to walk over commits based on some filtering that is defined
+			try (RevWalk walk = new RevWalk(repository)) {
+				RevCommit commit = walk.parseCommit(head.getObjectId());
 
-                // Print last commit message in master
-                System.out.println("\nCommit-Message: " + commit.getFullMessage());
+				// Print last commit message in master
+				System.out.println("\nCommit-Message: " + commit.getFullMessage());
 
-                walk.dispose();
-            }
-        }
-        catch (IOException e) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!");
-            System.out.println(e);
-        }
+				walk.dispose();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		catch (IOException e) {
+			System.out.println("!!!!!!!!!!!!!!!!!!!!");
+			System.out.println(e);
+		}
 		/*catch (GitAPIException e) {
 			System.out.println("!!!!!!!!!!!!!!!!!!!!");
 			System.out.println(e);
 		}*/
-    }
+	}
 }
