2be24e5209e2bf3fbda4fe3a3b21ed365e7103d7
VitalyBlack <Vitaly1Black@yandex.ru>
2021-05-19 21:40:34 +0300

    console work

--- linearizer_console/src/main/java/console/App.java
+++ linearizer_console/src/main/java/console/App.java
@@ -3,9 +3,5 @@ package console;
 import lin_core.Linearizer;
 
 public class App {
-    public static void main(String[] args) {
-        //System.out.println(new App().getGreeting());
-        Main main = new Main();
-        main.openrepo();
-    }
+
 }
--- linearizer_console/src/main/java/console/Main.java
+++ linearizer_console/src/main/java/console/Main.java
@@ -2,6 +2,14 @@ package console;
 
 import java.io.IOException;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.lang.System.out;
+
+import lin_core.Linearizer;
 import org.apache.commons.io.FileUtils;
 
 //import org.eclipse.jgit.api.Git;
@@ -11,45 +19,70 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 
 // Parts taken from this repo:
 // https://github.com/centic9/jgit-cookbook/tree/master/src/main/java/org/dstadler/jgit
 
 public class Main {
-    public void openrepo() {
-        // Путь к репозиторию (пока хардкод)
-        File repoDir = new File("C:/rapidjson/.git");
-        // File repoDir = new File("C:/Users/DDRDmakar/Documents/EDU/ТРПО/Tamagotchi-Atmega8");
 
-        // Creating repo object
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        try (Repository repository = builder.setGitDir(repoDir)
-                .readEnvironment() // Scan environment GIT_* variables
-                .findGitDir()      // Scan up the file system tree
-                .build()) {
-            System.out.println("Having repository: " + repository.getDirectory());
+    @Option(
+            name = "-h",
+            aliases = "--help",
+            usage = "Shows help screen.",
+            forbids = {"-l"}
+            )
+    private boolean help;
 
-            // The Ref holds an ObjectId for any type of object (tree, commit, blob, tree)
-            Ref head = repository.findRef("refs/heads/master");
-            System.out.println("Found head: " + head);
+    @Option(
+            name = "-l",
+            aliases = "--linearize",
+            usage = "Performs linearization between first and last commits, then puts it in a new fork."
+            )
+    private boolean lin;
 
-            // A RevWalk allows to walk over commits based on some filtering that is defined
-            try (RevWalk walk = new RevWalk(repository)) {
-                RevCommit commit = walk.parseCommit(head.getObjectId());
+    @Argument
+    private List<String> args = new ArrayList<String>();
 
-                // Print last commit message in master
-                System.out.println("\nCommit-Message: " + commit.getFullMessage());
+    public static void main(String[] args) throws Exception {
 
-                walk.dispose();
-            }
-        }
-        catch (IOException e) {
-            System.out.println("!!!!!!!!!!!!!!!!!!!!");
-            System.out.println(e);
-        }
-		/*catch (GitAPIException e) {
-			System.out.println("!!!!!!!!!!!!!!!!!!!!");
-			System.out.println(e);
-		}*/
+        Main main = new Main();
+        main.parseArgs(args);
     }
+
+    public void parseArgs(String[] args) throws Exception {
+        final CmdLineParser parser = new CmdLineParser(this);
+
+        if (args.length < 1) {
+            parser.printUsage(out);
+            System.exit(-1);
+        }
+        try {
+            parser.parseArgument(args);
+        } catch(CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("Example: linearizer -h");
+            System.err.println("Example: linearizer -l <repoPath> <start> [-s]");
+        }
+
+        if(help){
+            parser.printUsage(out);
+        }
+
+
+        if(lin){
+            String[] emptyParams = new String[0];
+            Map<String, String[]> settings = new TreeMap<>();
+            settings.put("badStarts", new String[] {"*", "+"});
+            settings.put("strip", emptyParams);
+            settings.put("fixCase", emptyParams);
+            Linearizer.processRepo("D:\\git_linearizer\\tests\\git_test_simple\\.git", "refs/heads/master", "e40fc2fbea20214634e22445d2339e59b5067017", settings  );
+        }
+
+    }
+
 }
