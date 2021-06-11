962151b2e9b14e33cdc86ea4334feb0d5c70ddc6
VitalyBlack <Vitaly1Black@yandex.ru>
2021-06-11 18:18:16 +0300

    CLI fiX

--- linearizer_console/src/main/java/console/Main.java
+++ linearizer_console/src/main/java/console/Main.java
@@ -1,7 +1,5 @@
 package console;
 
-import java.io.IOException;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,32 +8,20 @@ import java.util.TreeMap;
 import static java.lang.System.out;
 
 import lin_core.Linearizer;
-import org.apache.commons.io.FileUtils;
 
-//import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.spi.StringArrayOptionHandler;
 
-
-// Parts taken from this repo:
-// https://github.com/centic9/jgit-cookbook/tree/master/src/main/java/org/dstadler/jgit
-
 public class Main {
 
     @Option(
             name = "-h",
             aliases = "--help",
-            usage = "Shows help screen.",
-            forbids = {"-l"}
+            forbids ={"-l"},
+            usage = "Shows help screen."
             )
     private boolean help;
 
@@ -43,7 +29,7 @@ public class Main {
             name = "-l",
             aliases = "--linearize",
             handler = StringArrayOptionHandler.class,
-            required = true,
+            forbids = {"-h"},
             usage = "Performs linearization between first and last commits, then puts it in a new fork."
             )
     private List<String> list;
@@ -59,17 +45,17 @@ public class Main {
 
     public void parseArgs(String[] args) throws Exception {
         final CmdLineParser parser = new CmdLineParser(this);
-
-        if (args.length < 1) {
-            parser.printUsage(out);
-            System.exit(-1);
-        }
         try {
             parser.parseArgument(args);
+
+            if (args.length < 1) {
+                System.err.println("Incorrect input. No arguments received. See the example");
+                System.err.println("linearizer [-h|-l] arguments...");
+                System.exit(-1);
+            }
+
         } catch(CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("Example: linearizer -h");
-            System.err.println("Example: linearizer -l <repoPath> <start> [-s]");
         }
 
         if(help){
@@ -77,13 +63,22 @@ public class Main {
         }
 
 
-        if(list.isEmpty() == false){
-            String[] emptyParams = new String[0];
-            Map<String, String[]> settings = new TreeMap<>();
-            settings.put("badStarts", new String[] {"*", "+"});
-            settings.put("strip", emptyParams);
-            settings.put("fixCase", emptyParams);
-            Linearizer.processRepo(list.get(0), list.get(1), list.get(2), settings  );
+        if(list != null){
+            try {
+                if (list.size() != 4) {
+                    System.err.println("Incorrect input. See the example");
+                    System.err.println("Linearizer -l <repo_path> <branch> <start_commit> <settings>");
+                    System.exit(-1);
+                }
+                String[] emptyParams = new String[0];
+                Map<String, String[]> settings = new TreeMap<>();
+                settings.put("badStarts", new String[]{"*", "+"});
+                settings.put("strip", emptyParams);
+                settings.put("fixCase", emptyParams);
+                Linearizer.processRepo(list.get(0), list.get(1), list.get(2), settings);
+            } catch(CmdLineException e){
+                System.err.println(e.getMessage());
+            }
         }
 
     }
