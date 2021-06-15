0f3b38da0baccd35679a702cf5f72e3b7fd69408
RafaOstrovskiy <73179185+RafaOstrovskiy@users.noreply.github.com>
2021-06-15 13:07:38 +0300

    add integration for checkbox

--- src/functions.js
+++ src/functions.js
@@ -10,7 +10,7 @@ module.exports = {
 function helpFunc() {
     const { exec } = require("child_process");
 
-exec("Linearizer -h", (error, stdout, stderr) => {
+exec("Linearizer -h ", (error, stdout, stderr) => {
     if (error) {
         vscode.window.showInformationMessage(`error: ${error.message}`);
         return;
@@ -23,20 +23,73 @@ exec("Linearizer -h", (error, stdout, stderr) => {
 });
 };
 function linFunc(rep, start, branch, strip, fix_case, remove, fix_messages) {
+    const { exec } = require("child_process")
     if (rep == '' || start == '' || branch == '')
         vscode.window.showInformationMessage('Fix your lines')
     else {
-        vscode.window.showInformationMessage(' Repository: ' + rep +
-            ' Start: ' + start +
-            ' Branch: ' + branch);
+        exec("Linearizer -l " + rep + " " + branch + " " + start, (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showInformationMessage(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showInformationMessage(`${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`${stdout}`);
+        });
         if (strip == true)
-            vscode.window.showInformationMessage('strip is on')
+        exec("Linearizer -l " + rep + branch + start + "-t", (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showInformationMessage(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showInformationMessage(`${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`strip is on ${stdout}`);
+        });
+        // vscode.window.showInformationMessage('strip is on')
         if (fix_case == true)
-            vscode.window.showInformationMessage('fix_case is on')
+        exec("Linearizer -l " + rep + branch + start + "-f", (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showInformationMessage(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showInformationMessage(`${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`fix_case is on ${stdout}`);
+        });
+        // vscode.window.showInformationMessage('fix_case is on')
         if (remove == true)
-            vscode.window.showInformationMessage('remove is on')
+        exec("Linearizer -l " + rep + " " + branch + " " + start + " " + "-r", (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showInformationMessage(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showInformationMessage(`${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`remove is on ${stdout}`);
+        });
+        //vscode.window.showInformationMessage('remove is on')
         if (fix_messages == true)
-            vscode.window.showInformationMessage('fix_messages is on')
+        exec("Linearizer -l " + rep + branch + start + "-m", (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showInformationMessage(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                vscode.window.showInformationMessage(`${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`fix_messages is on ${stdout}`);
+        });
+        //vscode.window.showInformationMessage('fix_messages is on')
     }
 };
 function hello() {
@@ -46,7 +99,7 @@ function hello() {
 function help() {
     const { exec } = require("child_process");
 
-    exec("Linearizer -h", (error, stdout, stderr) => {
+    exec("Linearizer -h ", (error, stdout, stderr) => {
         if (error) {
             vscode.window.showInformationMessage(`error: ${error.message}`);
             return;
