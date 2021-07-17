de4fc2fcec15249d13207946bf1759ae4e36206f
DDRDmakar <makarevich.98@mail.ru>
2021-07-17 19:19:32 +0300

    cmd file fixed for linux

--- Linearizer.cmd
+++ Linearizer.cmd
@@ -1,3 +1,10 @@
-:;CMDSCRIPT
-@ECHO OFF 
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+java -jar ./linearizer_console/build/libs/Linearizer.jar "$@"
+exit $?
+
+:CMDSCRIPT
 java -jar ./linearizer_console/build/libs/Linearizer.jar %*
